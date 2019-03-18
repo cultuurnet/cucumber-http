5961c8f0c9d5e790a31e76b20db556557afa37bb
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-03-18 12:32:13 +0100

    Order world extensions alphabetically

--- lib/cucumber/http/world_extensions.rb
+++ lib/cucumber/http/world_extensions.rb
@@ -1,10 +1,10 @@
 require_relative 'world_extensions/json_spec_interface'
-require_relative 'world_extensions/url'
 require_relative 'world_extensions/headers'
 require_relative 'world_extensions/parameters'
 require_relative 'world_extensions/payload'
 require_relative 'world_extensions/request'
 require_relative 'world_extensions/response'
+require_relative 'world_extensions/url'
 
 World(JsonSpecInterface)
 World(Cucumber::Http::Headers)
